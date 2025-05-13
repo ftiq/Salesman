022c82825522869d5627e89d67c8237c0481df8f
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 01:37:36 +0300

    Update salesman.py

--- salesman_backend/models/salesman.py
+++ salesman_backend/models/salesman.py
@@ -5,9 +5,3 @@ class SalesmanProfile(models.Model):
     _description = 'بطاقة المندوب'
 
     name = fields.Char(string='اسم المندوب', required=True)
-    
-    # يمكن إضافة الحقول الأخرى لاحقاً بعد التأكد من عمل الأساسيات
-    # mobile = fields.Char(string='موبايل')
-    # branch = fields.Many2one('res.branch', string='الفرع')
-    # customer_ids = fields.Many2many('res.partner', string='العملاء')
-    # route_ids = fields.One2many('salesman.route', 'salesman_id', string='المسارات')
