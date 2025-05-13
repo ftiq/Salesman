a15d611da5cdceb780963c9ba6275efced70706b
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 00:27:27 +0300

    Delete sales_visit_manager directory

--- sales_visit_manager/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# sales_visit_manager/models/__init__.py
-from . import sales_visit
-from . import res_partner
-from . import account_move
-from . import account_payment
--- sales_visit_manager/models/account_payment.py
+++
@@ -1,7 +0,0 @@
-# sales_visit_manager/models/account_payment.py
-from odoo import models, fields
-
-class AccountPayment(models.Model):
-    _inherit = 'account.payment'
-
-    salesman_id = fields.Many2one('res.users', string='Salesman')
