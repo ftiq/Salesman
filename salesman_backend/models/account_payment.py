c1a46ddd901f1a060a9d6915241ed39956865857
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 01:09:29 +0300

    Create account_payment.py

---
+++ salesman_backend/models/account_payment.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class AccountPayment(models.Model):
+    _inherit = 'account.payment'
+    
+    salesman_id = fields.Many2one(
+        'salesman.profile',
+        string='المندوب'
+    )
