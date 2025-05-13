ab5e834226be821b02581537e5de3157f067c9db
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 01:15:00 +0300

    Update daily_route.py

--- salesman_backend/models/daily_route.py
+++ salesman_backend/models/daily_route.py
@@ -1,4 +1,4 @@
-from odoo import api, models, fields  # أضيف api هنا
+from odoo import api, models, fields
 
 class DailyRoute(models.Model):
     _name = 'salesman.daily.route'
@@ -7,7 +7,12 @@ class DailyRoute(models.Model):
     name = fields.Char(string='الاسم', compute='_compute_name')
     date = fields.Date(string='التاريخ', required=True)
     salesman_id = fields.Many2one('salesman.profile', string='المندوب')
-    route_line_ids = fields.One2many('salesman.daily.route.line', 'daily_route_id', string='العملاء')
+    
+    route_line_ids = fields.One2many(
+        'salesman.daily.route.line',  # تأكد من تطابق اسم النموذج
+        'daily_route_id',  # يجب أن يتطابق مع الحقل في النموذج الفرعي
+        string='تفاصيل الزيارات'
+    )
 
     @api.depends('date', 'salesman_id')
     def _compute_name(self):
