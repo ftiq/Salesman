4b2e68ca4018b956e6ece8f50e0b7262fef75879
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 00:38:12 +0300

    Update route.py

--- salesman_backend/models/route.py
+++ salesman_backend/models/route.py
@@ -1,15 +1,9 @@
+from odoo import models, fields
+
 class SalesmanRoute(models.Model):
     _name = 'salesman.route'
-    _description = 'مسار'
+    _description = 'مسار المندوب'
 
     name = fields.Char(string='اسم المسار', required=True)
     salesman_id = fields.Many2one('salesman.profile', string='المندوب')
     customer_ids = fields.Many2many('res.partner', string='العملاء')
-
-class DailyRoute(models.Model):
-    _name = 'salesman.daily.route'
-    _description = 'خط السير اليومي'
-
-    date = fields.Date(string='التاريخ', default=fields.Date.today)
-    salesman_id = fields.Many2one('salesman.profile', string='المندوب')
-    visit_ids = fields.One2many('salesman.visit.log', 'daily_route_id', string='الزيارات')
