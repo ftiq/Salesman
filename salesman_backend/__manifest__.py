45dfcd448f566dde807d4d249cddcc90fcb52e52
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 01:40:57 +0300

    Update __manifest__.py

--- salesman_backend/__manifest__.py
+++ salesman_backend/__manifest__.py
@@ -7,20 +7,6 @@
     'website': 'https://www.yourcompany.com',
     'category': 'Sales',
     'depends': ['base', 'sale', 'account', 'contacts'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/salesman_views.xml',
-        'views/route_views.xml',
-        'views/daily_route_views.xml',
-        'views/daily_route_line_views.xml',
-        'views/visit_views.xml',
-        'views/res_partner_views.xml',
-        'views/account_payment_views.xml',
-        'views/menu_views.xml',  # يجب أن يكون الأخير
-    ],
-    'demo': [
-        'demo/salesman_demo.xml',
-    ],
     'installable': True,
     'application': True,
     'auto_install': False,
