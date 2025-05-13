c5474a2301392bfd18b913c3280d678be6feb561
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 01:06:31 +0300

    Create res_partner.py

---
+++ salesman_backend/models/res_partner.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+    
+    salesman_id = fields.Many2one(
+        'salesman.profile',
+        string='المندوب المسؤول'
+    )
