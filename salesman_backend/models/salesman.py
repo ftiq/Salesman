04086ce596297d8ce7f759b5241fa806b01421b6
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 01:11:50 +0300

    Update salesman.py

--- salesman_backend/models/salesman.py
+++ salesman_backend/models/salesman.py
@@ -7,13 +7,14 @@ class SalesmanProfile(models.Model):
     name = fields.Char(string='اسم المندوب', required=True)
     mobile = fields.Char(string='موبايل')
     branch = fields.Many2one('res.branch', string='الفرع')
-    active = fields.Boolean(default=True)
     
-    # تعديل العلاقة لاستخدام نفس اسم الحقل
-    customer_ids = fields.One2many(
+    # تغيير العلاقة إلى Many2many كحل بديل
+    customer_ids = fields.Many2many(
         'res.partner',
-        'salesman_id',  # يجب أن يتطابق مع اسم الحقل في res.partner
-        string='العملاء'
+        string='العملاء',
+        relation='salesman_partner_rel',  # اسم جدول الربط
+        column1='salesman_id',
+        column2='partner_id'
     )
+    
     payment_ids = fields.One2many('account.payment', 'salesman_id', string='قسائم الدفع')
-    route_ids = fields.One2many('salesman.route', 'salesman_id', string='المسارات')
