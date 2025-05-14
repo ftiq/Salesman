e5e6bfd5b37dc9533f1854b95de6616850d97f5a
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-14 23:08:33 +0300

    Update __manifest__.py

--- salesman_backend/__manifest__.py
+++ salesman_backend/__manifest__.py
@@ -6,13 +6,21 @@
     'author': 'Your Company',
     'website': 'https://www.yourcompany.com',
     'category': 'Sales',
-    'depends': ['base', 'sale', 'account', 'contacts'],
+    'depends': ['base', 'sale', 'account', 'contacts', 'stock'],
     'installable': True,
     'application': True,
     'auto_install': False,
-    'data': [
-        'views/salesman_profile_view.xml',
-        # أضف المزيد من ملفات الـ XML هنا إن وجدت
-    ],
     'license': 'LGPL-3',
+    'data': [
+        'views/menu.xml',
+        'views/salesman_profile_view.xml',
+        'views/salesman_route_views.xml',
+        'views/salesman_daily_route_views.xml',
+        'views/salesman_visit_log_views.xml',
+        'views/sale_order_views.xml',
+        'views/account_payment_views.xml',
+        'views/account_move_views.xml',
+        'views/stock_picking_views.xml',
+        'views/stock_warehouse_views.xml',
+    ],
 }
