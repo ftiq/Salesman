e58a9b1202f3001f2acd66e04fe669753618d047
MHD Manar Zamrik <manar.z@ft.iq>
2025-05-16 01:25:01 +0300

    Delete salesman_backend directory

--- salesman_backend/models/stock_picking.py
+++
@@ -1,7 +0,0 @@
-# ملف: models/stock_picking.py
-from odoo import models, fields
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    salesman_id = fields.Many2one('salesman.profile', string="المندوب")
